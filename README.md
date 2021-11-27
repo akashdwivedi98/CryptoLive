2fb25dbb1701850b2ac4de0af81e4a0b8d08f5d1
Akash <akashdwivedi02@gmail.com>
2021-11-27 15:32:46 +0530

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# CryptoLive - Explore the World of Cryptocurrency
+
+![CryptoLive](https://ibb.co/D4Z78ZY)
+
+## Introduction
+
+Type (npm start) in the terminal to view the project.
+
---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoNewsHeaders = {
+  'x-bingapis-sdk': 'true',
+    'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+    'x-rapidapi-key': 'b071c8e479mshc1999f7424381d6p1560e4jsne754f80733f8',
+};
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: 'cryptoNewsApi',
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_NEWS_API_URL }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+    }),
+  }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
